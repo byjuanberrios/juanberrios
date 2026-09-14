c994b929b8bba452686ca2ea0d76fbd940b9f0ab
Juan Berrios <jberrioscifuentes@gmail.com>
2026-09-14 20:17:19 -0300

    refactor(shared/Header): adjust button width and layout
    Refactor the Header component to ensure buttons span the full width and improve layout consistency.
    
    Changes include:
    Updated button width to `w-full` to span the full width.
    Added `w-full` to the inner div to ensure it takes full width.
    Refactored button styling for better alignment and responsiveness.

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -27,9 +27,9 @@ const Header = () => {
       <div className="w-full header-lateral-spacing flex justify-center items-center mx-auto">
         <div className="relative overflow-hidden text-sm font-sans w-full rounded-3xl flex flex-wrap bg-stone-300/50 dark:bg-zinc-900/50 backdrop-blur-xs px-4.5 md:px-5 py-2.5 border border-neutral-400/20 dark:border-zinc-200/10 after:-z-10 after:h-full after:top-0 after:left-0 after:absolute after:w-full after:bg-linear-to-tr after:from-stone-400/20 after:via-neutral-500/10 after:to-zinc-400/20 after:blur-3xl">
           <div className="flex justify-between w-full items-center order-2 md:order-1">
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 w-full">
               <button
-                className="relative font-medium h-5 cursor-pointer before:content-[''] before:absolute before:-inset-x-1 before:-inset-y-3"
+                className="relative font-medium w-full h-5 cursor-pointer before:content-[''] before:absolute before:-inset-x-1 before:-inset-y-3"
                 onClick={() => setMenuOpen(!menuOpen)}
                 aria-expanded={menuOpen}
                 aria-controls="menu"
